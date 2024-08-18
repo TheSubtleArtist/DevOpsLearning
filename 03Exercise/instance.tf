b6ed71cf657b1aba76bb2ab16f5e47aacfe6ece8
thesubtleartist <thesubtleartist@yahoo.com>
2024-08-18 15:44:14 -0500

    Exercisee 03

--- 03Exercise/instance.tf
+++ 03Exercise/instance.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ex03-inst" {
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
   key_name               = aws_key_pair.dove-key.key_name
-  vpc_security_group_ids = ["sg-0780815f55104be8a"]
+  vpc_security_group_ids = ["sg-020c98cc025bcabd9"]
   tags = {
     Name    = "Exercise03-Instance"
     Project = "TerraformTutorial"
