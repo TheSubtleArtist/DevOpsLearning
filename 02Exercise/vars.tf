cac95b9d1c1c8ec257e5e84b79c0147c86f35b57
vagrant <vagrant@terraform>
2024-08-18 20:22:04 +0000

    Exercise02 Works

--- 02Exercise/vars.tf
+++ 02Exercise/vars.tf
@@ -1,13 +1,13 @@
-variable REGION {
+variable "REGION" {
   default = "us-east-1"
 }
 
-variable ZONE1 {
+variable "ZONE1" {
   default = "us-east-1b"
 }
 
-variable AMIS {
-  type = map
+variable "AMIS" {
+  type = map(any)
   default = {
     us-east-2 = "ami-0f5d333879b58a767"
     us-east-1 = "ami-0b1ca8b711f1d1fd7"
