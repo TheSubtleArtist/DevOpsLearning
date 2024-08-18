1fb960d4cbcf3b60b403b58b760dbd4d7042f9b4
thesubtleartist <thesubtleartist@yahoo.com>
2024-08-18 13:45:35 -0500

    add subnet_id

--- 01Exercise/01Intro.tf
+++ 01Exercise/01Intro.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "intro" {
   ami                    = "ami-0b1ca8b711f1d1fd7"
   instance_type          = "t2.micro"
-  availability_zone      = "us-east-1a"
+  availability_zone      = "us-east-1b"
   subnet_id              = "subnet-0d6de4f0e7a758d45"
   key_name               = "18Terraformkey.pem"
   vpc_security_group_ids = ["sg-011080c64a370f323"]
