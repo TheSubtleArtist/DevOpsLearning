efb70b8bf5a73ca3de90db3b04463f6e54f70eb9
thesubtleartist <thesubtleartist@yahoo.com>
2024-08-18 15:17:03 -0500

    Exercise 02

--- 02Exercise/instance.tf
+++ 02Exercise/instance.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "dove-inst" {
+resource "aws_instance" "ex02-inst" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
-  key_name               = "new-dove"
+  key_name               = "18TerFrmKey"
   subnet_id              = "subnet-0d6de4f0e7a758d45"
   vpc_security_group_ids = ["sg-011080c64a370f323"]
   tags = {
-    Name    = "Dove-Instance"
-    Project = "Dove"
+    Name    = "Ecercise02-Instance"
+    Project = "Exercise02"
   }
 }
