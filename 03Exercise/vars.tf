8599dab92e993c22b08f6989bffa3eab581a0cf8
thesubtleartist <thesubtleartist@yahoo.com>
2024-08-18 15:28:16 -0500

    Exercise03

---
+++ 03Exercise/vars.tf
@@ -0,0 +1,19 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1b"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-2 = "ami-0f5d333879b58a767"
+    us-east-1 = "ami-0b1ca8b711f1d1fd7"
+  }
+}
+
+variable USER {
+  default = "ec2-user"
+}
