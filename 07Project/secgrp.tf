66591ff558b02890fd146a4f5186ad69a101fd6a
thesubtleartist <thesubtleartist@yahoo.com>
2024-08-18 18:43:30 -0500

    final project

--- 07Project/secgrp.tf
+++ 07Project/secgrp.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "vprofile-bean-elb-sg" {
   vpc_id      = module.vpc.vpc_id
   egress {
     from_port   = 0
-    protocol    = "-1"
+    protocol    = "-1" # -1 is all protocols
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
