66a2dc53f908ed6c15c14008fd480a40e5a42c0f
thesubtleartist <thesubtleartist@yahoo.com>
2024-08-19 18:52:37 -0500

    many outdated resources, needs much updating

--- 07Project/bean-env.tf
+++ 07Project/bean-env.tf
@@ -1,5 +1,5 @@
 resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
-  name                = "vprofile-bean-prod-thesubtleartist"
+  name                = "vprofile-bean-prod-thesubtleartist" # must be unique
   application         = aws_elastic_beanstalk_application.vprofile-prod.name
   solution_stack_name = "64bit Amazon Linux 2 v4.7.0 running Tomcat 9 Corretto 11"
   cname_prefix        = "vprofile-bean-prod-thesubtleartist" # must be unique
