0fa26453970b79b0fab37e7077a4d687ca483325
thesubtleartist <thesubtleartist@yahoo.com>
2024-08-18 19:35:13 -0500

    webkafe works

--- setup-webkafe.sh
+++ setup-webkafe.sh
@@ -8,6 +8,11 @@ systemctl enable httpd
 cd /tmp
 wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
 unzip -o 2137_barista_cafe.zip
-mkdir /var/www/html/
+mkdir -p /var/www/html/
 cp -r 2137_barista_cafe/* /var/www/html/
 systemctl restart httpd
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --add-service=http --permanent --zone=public
+sudo firewall-cmd --reload
