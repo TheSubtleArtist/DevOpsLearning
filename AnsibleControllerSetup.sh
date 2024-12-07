6731b21b5f4b9fd03b0342987c9e7b689410a3be
thesubtleartist <thesubtleartist@yahoo.com>
2024-12-06 22:38:12 -0600

    no comment

---
+++ AnsibleControllerSetup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt-get install -y unzip git 
+
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
+
+
+
+sudo apt-get install -y python3-boto3 python3-botocore python3-boto vim
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+git config --global user.name = thesubtleartist
+git config --global user.email = thesubtleartist@yahoo.com
+
+#git clone https://github.com/TheSubtleArtist/AnsibleForAwsPractice.git
+
+#echo 'y' | sudo ufw enable
+#sudo ufw allow 22/tcp
+#sudo ufw reload
+
